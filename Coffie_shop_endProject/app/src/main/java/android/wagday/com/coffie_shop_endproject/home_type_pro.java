a10826f414a1d4df47d607162f72a570e0239706
Rashid <rashid201143@gmail.com>
2021-01-27 04:53:23 +0300

    اضافة صورة الشخصية

--- Coffie_shop_endProject/app/src/main/java/android/wagday/com/coffie_shop_endproject/home_type_pro.java
+++ Coffie_shop_endProject/app/src/main/java/android/wagday/com/coffie_shop_endproject/home_type_pro.java
@@ -1,7 +1,10 @@
 package android.wagday.com.coffie_shop_endproject;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -17,6 +20,11 @@ public class home_type_pro extends AppCompatActivity {
         setContentView(R.layout.activity_home_type_pro);
         img_accou = (ImageView)findViewById(R.id.img_acc);
         name_acc=findViewById(R.id.name_acc);
+        if(!MainActivity.users.getImges().equals("nu"))
+        {
+            Bitmap bt = ConvertBase64Toimages(MainActivity.users.getImges());
+            img_accou.setImageBitmap(bt);
+        }
         name_acc.setText(MainActivity.users.getFirst_name()+"  "+MainActivity.users.getLast_name());
         img_accou.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +34,14 @@ public class home_type_pro extends AppCompatActivity {
             }
         });
 
+    }
+    // تحويل النص الى صوره
+    public Bitmap ConvertBase64Toimages(String img){
+
+        byte[] decodedString = Base64.decode(img, Base64.DEFAULT);
+        Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+        System.out.println("The size of bitmap image agter low converting is ::::::::::::::::::::::::::::::::::::::::"+decodedByte.getByteCount());
+
+        return decodedByte;
     }
 }
